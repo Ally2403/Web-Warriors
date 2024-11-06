709c43dde51014a52fad67604643155a5eecdc86
unknown <allisonruiz@uninorte.edu.co>
2024-11-05 23:19:40 -0500

    Frames List to Data Structures

--- WebWarriors/GifPlayer.py
+++ WebWarriors/GifPlayer.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+from ListaSimple import *
 
 class GIFPlayer:
     def __init__(self, folder, num_frames, x=0, y=0, width=100, height=100, frame_delay=5, speed=5):
-        self.frames = []  # Lista para almacenar los cuadros
+        self.frames = ListaSimple()  # Lista para almacenar los cuadros
         self.num_frames = num_frames  # Número total de cuadros
         self.frame_index = 0  # Índice del cuadro actual
         self.x = x  # Posición en x
@@ -18,7 +19,7 @@ class GIFPlayer:
             filename = str(folder) + str(i) + ".png"
             frame = loadImage(filename)
             frame.resize(self.width, self.height)  # Redimensiona el cuadro si es necesario
-            self.frames.append(frame)
+            self.frames.agregar(frame)
 
     def display(self):
         noFill()
@@ -26,7 +27,9 @@ class GIFPlayer:
         rect(self.x + 22, self.y + 21, self.width - 50, self.height - 50)
         
         """Dibuja el cuadro actual en la posición especificada."""
-        image(self.frames[self.frame_index], self.x, self.y)
+        current_frame = self.frames.obtener(self.frame_index)  # Obtener el cuadro actual de la lista
+        if current_frame:
+            image(current_frame, self.x, self.y)
         # Avanza al siguiente cuadro
         self.delay_counter += 1
         # Avanza al siguiente cuadro solo si se alcanza el retraso deseado
